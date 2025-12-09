1b4d499a995619ac71b963a8e27aaae801778208
FeruYasu <fernando_yasumoto@hotmail.com>
2025-12-09 15:41:11 -0300

    footer mobile

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,12 +9,10 @@ import alcance from '@/img/alcance.png';
 import logoFooter from '@/img/logo-footer.png';
 
 export function Footer() {
-
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.grid}>
-
           <Image
             src={logoFooter}
             alt="Logo do Innova Imobiliária Digital"
@@ -26,54 +24,81 @@ export function Footer() {
           {/* Links Rapidos */}
           <div>
             <ul className={styles.linkList}>
-              <li><Link href="/" className={styles.link}>Início</Link></li>
-              <li><Link href="/imoveis?type=rent" className={styles.link}>Alugar</Link></li>
-              <li><Link href="/imoveis?type=sale" className={styles.link}>Comprar</Link></li>
-              <li><Link href="/anunciar" className={styles.link}>Anunciar</Link></li>
-              <li><Link href="/trabalhe-conosco" className={styles.link}>Trabalhe Conosco</Link></li>
-              <li><Link href="/area-do-cliente" className={styles.link}>Área do cliente</Link></li>
+              <li>
+                <Link href="/" className={styles.link}>
+                  Início
+                </Link>
+              </li>
+              <li>
+                <Link href="/imoveis?type=rent" className={styles.link}>
+                  Alugar
+                </Link>
+              </li>
+              <li>
+                <Link href="/imoveis?type=sale" className={styles.link}>
+                  Comprar
+                </Link>
+              </li>
+              <li>
+                <Link href="/anunciar" className={styles.link}>
+                  Anunciar
+                </Link>
+              </li>
+              <li>
+                <Link href="/trabalhe-conosco" className={styles.link}>
+                  Trabalhe Conosco
+                </Link>
+              </li>
+              <li>
+                <Link href="/area-do-cliente" className={styles.link}>
+                  Área do cliente
+                </Link>
+              </li>
             </ul>
           </div>
 
           {/* Info */}
           <div>
             <div className={styles.linkList}>
-              <h5>Innova Imobiliária Digital <br/> Creci J06584</h5>
-              <p>R. Anne Frank, 2132 <br/>Hauer - Curitiba/PR</p>
-              <p>Locação <br/> (41) 98701-0407</p>
-              <p>Vendas <br/> (41) 98701-0407</p>
+              <h5>
+                Innova Imobiliária Digital <br /> Creci J06584
+              </h5>
+              <p>
+                R. Anne Frank, 2132 <br />
+                Hauer - Curitiba/PR
+              </p>
+              <p>
+                Locação <br /> (41) 98701-0407
+              </p>
+              <p>
+                Vendas <br /> (41) 98701-0407
+              </p>
             </div>
           </div>
 
           {/* Contato */}
           <div className={styles.right}>
-            <div className={styles.icons}>
-              <Link href="https://www.instagram.com/innovaimobiliaria/" target="_blank">
-                <Image
-                  src={instagram}
-                  alt="Instagram"
-                  width={30}
-                  height={30}
-                />
+            <div className={`${styles.icons} ${styles.socials}`}>
+              <Link
+                href="https://www.instagram.com/innovaimobiliaria/"
+                target="_blank"
+              >
+                <Image src={instagram} alt="Instagram" width={30} height={30} />
               </Link>
-              <Link href="https://www.facebook.com/innovaimobiliariadigital/" target="_blank">
-                <Image
-                  src={facebook}
-                  alt="Facebook"
-                  width={30}
-                  height={30}
-                />
+              <Link
+                href="https://www.facebook.com/innovaimobiliariadigital/"
+                target="_blank"
+              >
+                <Image src={facebook} alt="Facebook" width={30} height={30} />
               </Link>
-              <Link href="https://www.linkedin.com/company/innovaimobiliaria/" target="_blank">
-                <Image
-                  src={linkedin}
-                  alt="LinkedIn"
-                  width={30}
-                  height={30}
-                />
+              <Link
+                href="https://www.linkedin.com/company/innovaimobiliaria/"
+                target="_blank"
+              >
+                <Image src={linkedin} alt="LinkedIn" width={30} height={30} />
               </Link>
             </div>
-            <div className={styles.icons}>
+            <div className={`${styles.icons} ${styles.partners}`}>
               <Image
                 src={pequeno}
                 alt="Logo do Pequeno Principe"
@@ -92,7 +117,9 @@ export function Footer() {
 
         {/* Copyright */}
         <div className={styles.copyright}>
-          <Link href="https://namiconsultoria.com.br/" target="_blank">Desenvolvido por Nami Consultoria</Link>
+          <Link href="https://namiconsultoria.com.br/" target="_blank">
+            Desenvolvido por Nami Consultoria
+          </Link>
         </div>
       </div>
     </footer>
